e1fbf2f988d718153b4c233e524a5349e04aacaf
saifkazi25 <m.saifkazi@gmail.com>
2025-07-07 22:26:08 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import dynamic from "next/dynamic";
+'use client';
 
-const QuizForm = dynamic(() => import("../components/QuizForm"), { ssr: false });
+import WebcamCapture from '@/components/WebcamCapture';
 
-export default function Home() {
+export default function SelfiePage() {
+  const handleCapture = (image: string) => {
+    console.log("Captured image:", image);
+    // TODO: Save to state, localStorage or send to backend
+  };
+
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <QuizForm />
+    <main className="flex items-center justify-center min-h-screen p-4">
+      <WebcamCapture onCapture={handleCapture} />
     </main>
   );
 }
