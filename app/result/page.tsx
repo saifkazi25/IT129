09a48603d9968f435408680699d8bd8494c5f264
saifkazi25 <m.saifkazi@gmail.com>
2025-07-08 15:35:09 +0300

    Update page.tsx

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import ResultDisplay from "../components/ResultDisplay";
+import ResultDisplay from "@/components/ResultDisplay";
 
 export default function ResultPage() {
   return (
