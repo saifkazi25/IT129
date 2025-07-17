dbc7b553fcb0288cb7fca87d2fb28cd99ad1bab7
saifkazi25 <m.saifkazi@gmail.com>
2025-07-17 13:23:29 +0300

    Update cloudinary.ts

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,18 +1,14 @@
 import axios from "axios";
 
+const CLOUD_NAME = "djm1jppes";
+const UPLOAD_PRESET = "infinite_tsukuyomi";
+
 export default async function uploadToCloudinary(base64Image: string) {
-  const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
-  const uploadPreset = "infinite_tsukuyomi";
-
-  if (!cloudName) {
-    throw new Error("Missing CLOUDINARY_CLOUD_NAME environment variable");
-  }
-
-  const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
+  const url = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
 
   const formData = new FormData();
   formData.append("file", base64Image);
-  formData.append("upload_preset", uploadPreset);
+  formData.append("upload_preset", UPLOAD_PRESET);
 
   const response = await axios.post(url, formData);
   return response.data.secure_url;
