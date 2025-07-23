d7c3de88f47d6db329f4307fa460aa4bdd2eea70
saifkazi25 <m.saifkazi@gmail.com>
2025-07-23 12:40:44 +0300

    Update cloudinary.ts

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,4 +1,4 @@
-import cloudinary from "cloudinary";
+import cloudinary from 'cloudinary';
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -6,10 +6,13 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export async function uploadToCloudinary(base64Image: string): Promise<string> {
-  const result = await cloudinary.v2.uploader.upload(`data:image/jpeg;base64,${base64Image}`, {
-    folder: "infinite_tsukuyomi",
+export async function uploadImageToCloudinary(imageUrl: string): Promise<string> {
+  console.log('☁️ Uploading to Cloudinary:', imageUrl);
+
+  const res = await cloudinary.v2.uploader.upload(imageUrl, {
+    folder: 'infinite_tsukuyomi',
   });
 
-  return result.secure_url;
+  console.log('✅ Cloudinary response:', res);
+  return res.secure_url;
 }
