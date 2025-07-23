c6eb2f5bb7cbf39ffb4b27d7a41e276d1dc408d8
saifkazi25 <m.saifkazi@gmail.com>
2025-07-23 12:33:42 +0300

    Update facefusion.ts

--- utils/facefusion.ts
+++ utils/facefusion.ts
@@ -4,29 +4,22 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!,
 });
 
-export async function mergeFaces({
-  targetImageUrl,
-  sourceImageUrl,
-}: {
-  targetImageUrl: string;
-  sourceImageUrl: string;
-}): Promise<string> {
+export async function mergeFaces(selfieUrl: string, fantasyUrl: string): Promise<string> {
   const output = await replicate.run(
     "lucataco/modelscope-facefusion:52edbb2b42beb4e19242f0c9ad5717211a96c63ff1f0b0320caa518b2745f4f7",
     {
       input: {
-        source_image: sourceImageUrl,
-        target_image: targetImageUrl,
-        sface_enhance: true,
-        use_croper: true,
+        target_image: fantasyUrl,
+        source_image: selfieUrl,
+        mode: "face_swap",
+        face_enhancer: true,
       },
     }
   );
 
   if (!output || typeof output !== "string") {
-    throw new Error("FaceFusion failed to return an image URL.");
+    throw new Error("FaceFusion failed to return a valid URL.");
   }
 
   return output;
 }
-
